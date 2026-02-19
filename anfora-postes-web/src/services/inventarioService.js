dd0e16cf527c07ff670b4793ec03bb8ccf3bd5d5
DaniellTarazona26 <tarazonadaniel1003@gmail.com>
2026-02-19 16:46:58 -0500

    railway5

--- anfora-postes-web/src/services/inventarioService.js
+++ anfora-postes-web/src/services/inventarioService.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = 'https://inventel-production.up.railway.app/api'
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('token')
