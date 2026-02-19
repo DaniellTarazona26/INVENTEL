40a77367be9a772f31d7fb1524d3e5385003dfa2
DaniellTarazona26 <tarazonadaniel1003@gmail.com>
2026-02-19 16:20:57 -0500

    railway3

--- anfora-postes-web/backend/config/database.js
+++ anfora-postes-web/backend/config/database.js
@@ -1,16 +1,9 @@
-// backend/config/database.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('connect', () => {
@@ -23,3 +16,4 @@ pool.on('error', (err) => {
 });
 
 module.exports = pool;
+
