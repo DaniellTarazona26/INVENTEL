4065db165e6713ca680ab0997bc45fcbe1be5c9c
DaniellTarazona26 <tarazonadaniel1003@gmail.com>
2026-02-19 16:32:09 -0500

    railway4

--- anfora-postes-web/src/services/api.js
+++ anfora-postes-web/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'inventel-production.up.railway.app';
+const API_URL = 'https://inventel-production.up.railway.app/api';
 
 // Crear instancia de axios
 const api = axios.create({
